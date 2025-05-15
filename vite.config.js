1440108c4f3dd43ffbcf878416a7c4482b3ac089
PRADEEP-RAJ-VS <146763897+PRADEEP-RAJ-VS@users.noreply.github.com>
2025-05-15 21:10:18 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
+  preview: {
+    // allow your Azure-hosted URL so that `vite preview` won’t block it
+    allowedHosts: [
+      "cloud-auto-gzcwg7ahcabqhrf2.centralindia-01.azurewebsites.net"
+    ]
+  }
+});
